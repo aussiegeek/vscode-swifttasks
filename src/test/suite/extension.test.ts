5ac3a0cfd5925f1cfba24ff31fcfac0e6362a338
Alan Harper <475+aussiegeek@users.noreply.github.com>
2020-02-18 23:35:53 +1100

    Add build task type
    Big refactoring along the way to represent tasks for targets & packages
    Moved generation for shell commands out of task generation

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -17,10 +17,15 @@ suite("Extension Test Suite", () => {
   test("tasks in source package are configured", async function() {
     this.timeout(5000);
     const tasks = await vscode.tasks.fetchTasks();
-    expect(tasks).to.have.lengthOf(1);
-    assertUnwrap(tasks[0], task => {
-      expect(task.name).to.eq("basicTests");
-      expect(task.source).to.eq("swift");
-    });
+    expect(tasks).to.have.lengthOf(6);
+
+    expect(tasks.map(task => task.name)).to.have.members([
+      "build package: no-tests",
+      "build target: no-tests",
+      "build package: basic",
+      "build target: basic",
+      "test target: basicTests",
+      "test package: basic"
+    ]);
   });
 });
--- src/types.ts
+++ src/types.ts
@@ -2,20 +2,51 @@ import * as vscode from "vscode";
 // see https://github.com/apple/swift-package-manager/blob/master/Sources/PackageModel/Manifest.swift
 
 export interface SwiftPackageDefinition {
-  name?: string;
+  name: string;
   targets: SwiftTarget[];
 }
 
-type SwiftTarget = {
-  type: "regular" | "test";
+export type SwiftTarget = SwiftTestTarget | SwiftBuildTarget;
+interface SwiftBaseTarget {
   name: string;
-};
+}
 
-export interface SwiftTaskDefinition extends vscode.TaskDefinition {
+export interface SwiftTestTarget extends SwiftBaseTarget {
+  type: "test";
+}
+
+export interface SwiftBuildTarget extends SwiftBaseTarget {
+  type: "regular";
+}
+export type SwiftTargetType = "regular" | "test";
+export type SwiftPackageTaskType = "build" | "test";
+export interface SwiftPackageTaskDefinition extends vscode.TaskDefinition {
   directory: string;
+  selector: SwiftSelectors;
+  taskType: SwiftPackageTaskType;
+  type: "swift";
+}
+export type SwiftSelectors = SwiftPackageSelector | SwiftTargetSelector;
+export interface SwiftPackageSelector {
+  kind: "package";
+}
+
+export interface SwiftTargetSelector {
+  kind: "target";
+  name: string;
 }
 
 export interface RawPackageDefinition {
   packageDefinition: SwiftPackageDefinition;
   directory: string;
+}
+
+export function isTask(task: vscode.Task | undefined): task is vscode.Task {
+  return task !== undefined && (task as vscode.Task).definition !== undefined;
+}
+
+export function isSwiftPackageDefinition(
+  definition: vscode.TaskDefinition | SwiftPackageTaskDefinition
+): definition is SwiftPackageTaskDefinition {
+  return (definition as SwiftPackageTaskDefinition).selector !== undefined;
 }
